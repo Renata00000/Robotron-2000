f1049f5cd4e1351fce12af0311f296c973321250
Renata <125274790+Renata00000@users.noreply.github.com>
2023-05-17 11:47:26 -0300

    Update main.js
    adicionando cliques ao Robo

--- robotron-2000/main.js
+++ robotron-2000/main.js
@@ -1,1 +1,16 @@
 
+const robotron = document.querySelector('#robotron');
+
+
+robotron.addEventListener('click', () => {
+    console.log('clique no robo');
+
+})
+
+
+function dizOi (nome){
+    console.log(nome);
+    console.log('Bem vindo ao Robotron 2000')
+}
+
+dizOi('Pedro')
